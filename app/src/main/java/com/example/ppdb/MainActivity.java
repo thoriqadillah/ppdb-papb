a6fffe9011ba0863b5784141a7efa5bf6d72ce1b
unknown <sandriansandy@gmail.com>
2021-11-24 17:09:52 +0700

    add comment

--- app/src/main/java/com/example/ppdb/MainActivity.java
+++ app/src/main/java/com/example/ppdb/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     public String a;
+    //test
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
