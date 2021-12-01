f7e9d620d36de273324d47690041612ef49a54cc
aednaas <aishaedna@student.ub.ac.id>
2021-12-01 15:36:49 +0700

    layout data diri

--- app/src/main/java/com/example/ppdb/dataDiri.java
+++ app/src/main/java/com/example/ppdb/dataDiri.java
@@ -1,4 +1,4 @@
-package com.example.papbb;
+package com.example.ppdb;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -6,29 +6,15 @@ import android.os.Bundle;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
+import com.example.ppdb.R;
+
 public class dataDiri extends AppCompatActivity {
 
-    private RadioGroup list_opsi;
 
-
-        @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_datadiri);
-            list_opsi = findViewById(R.id.opsi);
-            list_opsi.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-                @Override
-                public void onCheckedChanged(RadioGroup radioGroup, int id) {
-                    switch (id){
-                        case R.id.laki:
-                            Toast.makeText(getApplication(), "Laki-Laki", Toast.LENGTH_SHORT).show();
-                            break;
-                        case R.id.perempuan:
-                            Toast.makeText(getApplication(), "Perempuan", Toast.LENGTH_SHORT).show();
-                            break;
-                    }
-                }
-            });
+
 
     }
 }
