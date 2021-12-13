83c2e7f12ac141fb55cde826eb5c06d60b461aaa
FikriAlmas <fikrialmas71@gmail.com>
2021-12-13 20:39:07 +0700

    nyoba recyclerview

--- app/src/main/java/com/example/ppdb/PengumumanActivity.java
+++ app/src/main/java/com/example/ppdb/PengumumanActivity.java
@@ -1,16 +1,65 @@
 package com.example.ppdb;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.ppdb.adapter.Adapter;
+import com.example.ppdb.model.Siswa;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class PengumumanActivity extends AppCompatActivity {
+    FirebaseDatabase db = FirebaseDatabase.getInstance("https://ppdb-papb-1a3c3-default-rtdb.asia-southeast1.firebasedatabase.app");
+    DatabaseReference dbReference = db.getReference(Siswa.class.getSimpleName());
+
+    ArrayList<Siswa> list;
+    Adapter adapter;
+
+    private RecyclerView mRecylcer;
+    private LinearLayoutManager mManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pengumuman);
 
+        mRecylcer = findViewById(R.id.rvPengumuman);
+        mRecylcer.setHasFixedSize(true);
+
+        mManager = new LinearLayoutManager(this);
+//        mManager.setReverseLayout(true);
+//        mManager.setStackFromEnd(true);
+        mRecylcer.setLayoutManager(mManager);
+        list = new ArrayList<>();
+        adapter = new Adapter(this, list);
+        mRecylcer.setAdapter(adapter);
+
+
+        dbReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot dataSnapshot1: dataSnapshot.getChildren()){
+                    Siswa mhs = dataSnapshot1.getValue(Siswa.class);
+                    list.add(mhs);
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(getApplicationContext(), "Terjadi kesalahan",Toast.LENGTH_LONG).show();
+            }
+        });
 
     }
 }
