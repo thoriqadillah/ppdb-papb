4a0d19e2dcbd0a9c68c09792a668fae71239e900
unknown <sandriansandy@gmail.com>
2021-12-15 08:42:19 +0700

    edit nilai

--- app/src/main/java/com/example/ppdb/adapter/Adapter.java
+++ app/src/main/java/com/example/ppdb/adapter/Adapter.java
@@ -45,6 +45,7 @@ public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder> {
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
         Siswa siswa = siswas.get(position);
         holder.nama.setText(siswa.getNamaLengkap());
+        holder.nomor.setText(String.valueOf(position + 1));
 //        holder.nama.setText(siswas.get(position).getNamaLengkap());
     }
 
@@ -55,10 +56,12 @@ public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder> {
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
         TextView nama;
+        TextView nomor;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             nama = itemView.findViewById(R.id.txNamaPengumuman);
+            nomor=itemView.findViewById(R.id.txNomor);
         }
     }
 }
--- app/src/main/java/com/example/ppdb/model/Siswa.java
+++ app/src/main/java/com/example/ppdb/model/Siswa.java
@@ -5,8 +5,17 @@ import java.util.HashMap;
 public class Siswa {
 
     private String siswaId, namaLengkap, email, password, nisn, tglLahir, tempatLahir, agama, asalKota, asalSekolak, jenisKelamin, alamat;
+    private int nilaiMathUN, nilaiBindoUN, nilaiIpaUN, nilaiMathUS, nilaiBindoUS, nilaiIpaUS, nilaiIpsUS, rerataUN, rerataUS;
+    private static Siswa instatce;
 
-    public Siswa( String nisn, String namaLengkap, String jenisKelamin, String tglLahir, String tempatLahir, String agama, String alamat, String asalKota, String asalSekolak) {
+    public static Siswa getInstance() {
+        if (instatce == null) {
+            instatce = new Siswa();
+        }
+        return instatce;
+    }
+
+    private Siswa( String nisn, String namaLengkap, String jenisKelamin, String tglLahir, String tempatLahir, String agama, String alamat, String asalKota, String asalSekolak) {
         this.nisn = nisn;
         this.namaLengkap = namaLengkap;
         this.jenisKelamin = jenisKelamin;
@@ -18,14 +27,86 @@ public class Siswa {
         this.asalSekolak = asalSekolak;
     }
 
-    public Siswa(String namaLengkap, String email, String password) {
+    private Siswa(String namaLengkap, String email, String password) {
         this.namaLengkap = namaLengkap;
         this.email = email;
         this.password = password;
     }
 
-    public Siswa() {
+    private Siswa() {
 
+    }
+
+    public void setNilaiMathUN(int nilaiMathUN) {
+        this.nilaiMathUN = nilaiMathUN;
+    }
+
+    public void setNilaiBindoUN(int nilaiBindoUN) {
+        this.nilaiBindoUN = nilaiBindoUN;
+    }
+
+    public void setNilaiIpaUN(int nilaiIpaUN) {
+        this.nilaiIpaUN = nilaiIpaUN;
+    }
+
+    public void setNilaiMathUS(int nilaiMathUS) {
+        this.nilaiMathUS = nilaiMathUS;
+    }
+
+    public void setNilaiBindoUS(int nilaiBindoUS) {
+        this.nilaiBindoUS = nilaiBindoUS;
+    }
+
+    public void setNilaiIpaUS(int nilaiIpaUS) {
+        this.nilaiIpaUS = nilaiIpaUS;
+    }
+
+    public void setNilaiIpsUS(int nilaiIpsUS) {
+        this.nilaiIpsUS = nilaiIpsUS;
+    }
+
+    public void setRerataUN(int nilaiMathUN, int nilaiBindoUN, int nilaiIpaUN) {
+        this.rerataUN = (nilaiMathUN + nilaiBindoUN + nilaiIpaUN) / 3 ;
+    }
+
+    public void setRerataUS(int nilaiMathUS, int nilaiBindoUS, int nilaiIpaUS, int nilaiIpsUS) {
+        this.rerataUS = (nilaiMathUS + nilaiBindoUS + nilaiIpaUS + nilaiIpsUS) / 4;
+    }
+
+    public int getNilaiMathUN() {
+        return nilaiMathUN;
+    }
+
+    public int getNilaiBindoUN() {
+        return nilaiBindoUN;
+    }
+
+    public int getNilaiIpaUN() {
+        return nilaiIpaUN;
+    }
+
+    public int getNilaiMathUS() {
+        return nilaiMathUS;
+    }
+
+    public int getNilaiBindoUS() {
+        return nilaiBindoUS;
+    }
+
+    public int getNilaiIpaUS() {
+        return nilaiIpaUS;
+    }
+
+    public int getNilaiIpsUS() {
+        return nilaiIpsUS;
+    }
+
+    public int getRerataUN() {
+        return rerataUN;
+    }
+
+    public int getRerataUS() {
+        return rerataUS;
     }
 
     public String getAlamat() {
