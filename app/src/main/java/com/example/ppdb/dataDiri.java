85ad544af10e90b889cf718f983858a43e463ae1
unknown <sandriansandy@gmail.com>
2021-12-15 09:53:28 +0700

    scroll data diri

--- app/src/main/java/com/example/ppdb/dataDiri.java
+++ app/src/main/java/com/example/ppdb/dataDiri.java
@@ -122,26 +122,22 @@ public class dataDiri extends AppCompatActivity  implements View.OnClickListener
                     dbSiswa.child("nisn").setValue(siswa.getNisn());
                     Toast.makeText(this, "Data diri berhasil terupdate", Toast.LENGTH_SHORT).show();
                 }
-            } else {
-                Toast.makeText(this, "Data NISN tidak boleh kosong", Toast.LENGTH_SHORT).show();
             }
+
             if (namaLengkap != null) {
                 if (!namaLengkap.equals(inputNamalkp.getText().toString())) {
                     siswa.setNamaLengkap(inputNamalkp.getText().toString());
                     dbSiswa.child("namaLengkap").setValue(siswa.getNamaLengkap());
                     Toast.makeText(this, "Data diri berhasil terupdate", Toast.LENGTH_SHORT).show();
                 }
-            } else {
-                Toast.makeText(this, "Data Nama tidak boleh kosong", Toast.LENGTH_SHORT).show();
             }
+
             if (agama != null) {
                 if (!agama.equals(inputAgama.getText().toString())) {
                     siswa.setAgama(inputAgama.getText().toString());
                     dbSiswa.child("agama").setValue(siswa.getAgama());
                     Toast.makeText(this, "Data diri berhasil terupdate", Toast.LENGTH_SHORT).show();
                 }
-            } else {
-                Toast.makeText(this, "Data Agama tidak boleh kosong", Toast.LENGTH_SHORT).show();
             }
             if (alamat != null) {
                 if (!alamat.equals(inputAlamat.getText().toString())) {
@@ -149,8 +145,6 @@ public class dataDiri extends AppCompatActivity  implements View.OnClickListener
                     dbSiswa.child("alamat").setValue(siswa.getAlamat());
                     Toast.makeText(this, "Data diri berhasil terupdate", Toast.LENGTH_SHORT).show();
                 }
-            } else {
-                Toast.makeText(this, "Data Alamat tidak boleh kosong", Toast.LENGTH_SHORT).show();
             }
             if (asalKota != null) {
                 if (!asalKota.equals(inputKota.getText().toString())) {
@@ -158,8 +152,6 @@ public class dataDiri extends AppCompatActivity  implements View.OnClickListener
                     dbSiswa.child("asalKota").setValue(siswa.getAsalKota());
                     Toast.makeText(this, "Data diri berhasil terupdate", Toast.LENGTH_SHORT).show();
                 }
-            } else {
-                Toast.makeText(this, "Data Asal Kota tidak boleh kosong", Toast.LENGTH_SHORT).show();
             }
             if (asalSekolah != null) {
                 if (!asalSekolah.equals(inputAsalSekolah.getText().toString())) {
@@ -167,8 +159,6 @@ public class dataDiri extends AppCompatActivity  implements View.OnClickListener
                     dbSiswa.child("asalSekolak").setValue(siswa.getAsalSekolak());
                     Toast.makeText(this, "Data diri berhasil terupdate", Toast.LENGTH_SHORT).show();
                 }
-            } else {
-                Toast.makeText(this, "Data Asal Sekolah tidak boleh kosong", Toast.LENGTH_SHORT).show();
             }
             if (jenisKelamin != null) {
                 if (!jenisKelamin.equals(inputjenis.getText().toString())) {
@@ -176,8 +166,6 @@ public class dataDiri extends AppCompatActivity  implements View.OnClickListener
                     dbSiswa.child("jenisKelamin").setValue(siswa.getJenisKelamin());
                     Toast.makeText(this, "Data diri berhasil terupdate", Toast.LENGTH_SHORT).show();
                 }
-            } else {
-                Toast.makeText(this, "Data Jenis Kelamin tidak boleh kosong", Toast.LENGTH_SHORT).show();
             }
             if (tempatLahir != null) {
                 if (!tempatLahir.equals(inputTempatLahir.getText().toString())) {
@@ -185,8 +173,6 @@ public class dataDiri extends AppCompatActivity  implements View.OnClickListener
                     dbSiswa.child("tempatLahir").setValue(siswa.getTempatLahir());
                     Toast.makeText(this, "Data diri berhasil terupdate", Toast.LENGTH_SHORT).show();
                 }
-            } else {
-                Toast.makeText(this, "Data Tempat Lahir tidak boleh kosong", Toast.LENGTH_SHORT).show();
             }
             if (tglLahir != null) {
                 if (!tglLahir.equals(inputTanggalLahir.getText().toString())) {
@@ -194,8 +180,6 @@ public class dataDiri extends AppCompatActivity  implements View.OnClickListener
                     dbSiswa.child("tglLahir").setValue(siswa.getTglLahir());
                     Toast.makeText(this, "Data diri berhasil terupdate", Toast.LENGTH_SHORT).show();
                 }
-            } else {
-                Toast.makeText(this, "Data Tanggal Lahir tidak boleh kosong", Toast.LENGTH_SHORT).show();
             }
 
             if (nisn == null && namaLengkap == null && agama == null && alamat == null && asalKota == null
